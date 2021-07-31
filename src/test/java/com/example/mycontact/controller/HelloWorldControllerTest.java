f60396a48fe436743e11e542bd7044ecafbdb6ca
hiheehee <gkek41@naver.com>
2021-07-31 17:23:11 +0900

    MockMvc 테스트 만들기

--- src/test/java/com/example/mycontact/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/mycontact/controller/HelloWorldControllerTest.java
@@ -3,6 +3,11 @@ package com.example.mycontact.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +16,20 @@ class HelloWorldControllerTest {
     @Autowired
     private HelloWorldController helloWorldController;
 
+    private MockMvc mockMvc;
+
     @Test
     void helloWorld(){
         System.out.println(helloWorldController.helloWorld());
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/helloWorld"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("HelloWorld"));
     }
 }
