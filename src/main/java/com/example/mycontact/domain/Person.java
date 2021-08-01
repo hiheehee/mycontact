65e9e219278495fda7596a74cf9761cc03fb5734
hiheehee <gkek41@naver.com>
2021-08-01 12:09:31 +0900

    Lombok - @Constructor, @EqualsAndHashCode, @Data

--- src/main/java/com/example/mycontact/domain/Person.java
+++ src/main/java/com/example/mycontact/domain/Person.java
@@ -9,12 +9,10 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor // 기본 생성자
-@AllArgsConstructor // 모든 변수 포함하는 생성자
-@RequiredArgsConstructor // 반드시 있어야 하는 생성자 지정
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class Person {
     @Id
     @GeneratedValue
@@ -28,6 +26,7 @@ public class Person {
 
     private String hobby;
 
+    @NonNull
     private String bloodType;
 
     private String address;
@@ -37,21 +36,4 @@ public class Person {
     private LocalDateTime birthday;
 
     private String phoneNumber;
-
-    @Override
-    public boolean equals(Object o) {
-        if(o == null){
-            return false;
-        }
-        Person person = (Person) o;
-
-        if(!person.getName().equals(this.getName())) return false;
-        if(person.getAge() != this.getAge()) return false;
-        return  true;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age, hobby, bloodType, address, job, birthday, phoneNumber);
-    }
 }
--- src/test/java/com/example/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/mycontact/repository/PersonRepositoryTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
@@ -31,9 +33,15 @@ class PersonRepositoryTest {
 
     @Test
     void hashCodeEquals(){
-        Person person1 = new Person("martin",10);
-        Person person2 = new Person("martin",10);
+        Person person1 = new Person("martin", 10, "A");
+        Person person2 = new Person("martin", 10, "A");
 
         System.out.println(person1.equals(person2));
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
    }
 }
