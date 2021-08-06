1ce8809a9e45d448525ffb18e75d4319674510b3
hiheehee <gkek41@naver.com>
2021-08-06 23:39:38 +0900

    Refactoring - 리팩토링 도메인코드 #1

--- src/main/java/com/example/mycontact/domain/dto/Birthday.java
+++ src/main/java/com/example/mycontact/domain/dto/Birthday.java
@@ -14,16 +14,24 @@ import java.time.LocalDate;
 @Data
 public class Birthday {
     private Integer yearOfBirthday;
-    @Min(1)
-    @Max(12)
     private Integer monthOfBirthday;
-    @Min(1)
-    @Max(31)
     private Integer dayOfBirthday;
 
-    public Birthday(LocalDate birthday){
+    private Birthday(LocalDate birthday) {
         this.yearOfBirthday = birthday.getYear();
         this.monthOfBirthday = birthday.getMonthValue();
         this.dayOfBirthday = birthday.getDayOfMonth();
+    }
+
+    public static Birthday of(LocalDate birthday) {
+        return new Birthday(birthday);
+    }
+
+    public int getAge(){
+        return LocalDate.now().getYear() - this.yearOfBirthday + 1;
+    }
+
+    public boolean isBirthdayToday(){
+        return LocalDate.now().equals(LocalDate.of(yearOfBirthday,monthOfBirthday,dayOfBirthday));
     }
 }
