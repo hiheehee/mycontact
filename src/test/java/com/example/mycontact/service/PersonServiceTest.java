64d6f4373947936306c4613f778bcf9084fa6033
hiheehee <gkek41@naver.com>
2021-08-07 20:50:25 +0900

    Mock-Test - 02. Service Test #2

--- src/test/java/com/example/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/example/mycontact/service/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.mycontact.service;
 
+import com.example.mycontact.controller.dto.PersonDto;
 import com.example.mycontact.domain.Person;
 import com.example.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
@@ -7,12 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
@@ -31,5 +38,33 @@ class PersonServiceTest {
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPerson() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("martin")));
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person.getName()).isEqualTo("martin");
+    }
+
+    @Test
+    void getPersonifNotFount() {
+        when(personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        Person person = personService.getPerson(1L);
+
+        assertThat(person).isNull();
+    }
+
+    @Test
+    void put() {
+        PersonDto dto = PersonDto
+                .of("martin", "programming", "판교", LocalDate.now(), "programmer", "010-1111-2222");
+        personService.put(dto);
+        verify(personRepository, times(1)).save(any(Person.class));
     }
 }
